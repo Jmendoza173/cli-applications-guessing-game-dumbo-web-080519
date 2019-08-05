49c3ec626b23b370e41ad334de0c8719b8a68afb
Joel Mendoza <joelmendza173@gmail.com>
2019-08-05 00:57:45 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def run_guessing_game
-  unknown = rand(1..6)
+  unknown = rand(6)+1
   puts "Guess their own number between 1 and 6"
   picked = gets.chomp
   if picked == "exit"
